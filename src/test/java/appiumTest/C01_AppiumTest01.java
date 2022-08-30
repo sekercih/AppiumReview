d333424e8ac7e8ad3a3005bac3b01a05602af611
sekercih <67022880+sekercih@users.noreply.github.com>
2022-08-30 22:15:46 +0300

    desiredCapabilities

--- src/test/java/appiumTest/C01_AppiumTest01.java
+++ src/test/java/appiumTest/C01_AppiumTest01.java
@@ -17,21 +17,24 @@ public class C01_AppiumTest01 {
     public void test() throws MalformedURLException, InterruptedException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus 6");
-        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel3");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
         desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-        desiredCapabilities.setCapability(MobileCapabilityType.APP, "src/app/Duolingo.apk");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\Mehmet\\Desktop\\GuiderSoft Gün Package\\com.GuiderSoftAppium\\src\\app\\Duolingo.apk");
+       // desiredCapabilities.setCapability(MobileCapabilityType.FULL_RESET,true);
         // ! Yukarıdaki path i kendinize göre ayarlayınız.
+        //! emulator -list-avds => Cihaz ismi
+        //! adb shell getprop ro.build.version.release => android version
 
 
         AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http:127.0.0.1:4723/wd/hub"), desiredCapabilities);
-
+/*
         WebElement getStart=driver.findElementById("com.duolingo:id/introFlowNewUserButton");
         String yazi=getStart.getText();
         System.out.println("yazi = " + yazi);
         Thread.sleep(5000);
         getStart.click();
-
+*/
         Thread.sleep(5000);
 
 /*
